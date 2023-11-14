b1b7c28f5930fbf236ff45578146458d304c361e
Oliver Salzburg <oliver.salzburg@gmail.com>
2023-11-14 13:09:35 +0100

    feat: Modernize

--- readme.md
+++ readme.md
@@ -1,13 +1,19 @@
 # lit-i18n
+
 i18next lit-html directive (could possible add other i18n backends).
 
 [![Build Status](https://dev.azure.com/colscott/lit-i18n/_apis/build/status/colscott.lit-i18n?branchName=master)](https://dev.azure.com/colscott/lit-i18n/_build/latest?definitionId=2&branchName=master)
 
 ## Install
+
     npm install lit-i18n
+
 ## Usage
+
 Note: this library uses lit-html 2. If you use lit-html 1.x.x then please use lit-i18n 2.x.x.
+
 ### Config
+
 Nothing new here. Just use the usual i18next [config](https://www.i18next.com/overview/configuration-options). You can import from i18next.
 
     i18next.init({
@@ -29,11 +35,13 @@ Nothing new here. Just use the usual i18next [config](https://www.i18next.com/ov
     });
 
 ### Translations
+
 lit-i18n exposes two directives called translate and translateWhen.
 The translate directive has the same signature and functionality as the i18next [t method](https://www.i18next.com/overview/api#t). lit-i18n also exposes lit-htmls html and render methods.
 
-#### translate    
-    import { translate as t, html, render } from 'lit-i18n/src/lit-i18n.js';
+#### translate
+
+    import { translate as t, html, render } from 'lit-i18n/source/lit-i18n.js';
 
     /** @typedef {{name: string; age: number; male: boolean}} Person */
     class I18nElement extends HTMLElement {
@@ -74,12 +82,13 @@ The translate directive has the same signature and functionality as the i18next 
     }
 
 #### translateWhen directive - postponing translations until they have loaded
+
 Screen flicker can occur if you load multiple namespaces and run translations prior to the translation resource being loaded. I18next returns promises that will resolve after the resources are ready.
 If this happens you can use `translateWhen`.
 `translateWhen` differs to translate in that it also accepts a Promise. This Promise would typically be the Promise returned by `i18next.init` or `i18next.loadNamespaces` or `i18next.loadLanguages`. The translateWhen directive will not try to translate the key until the Promise is resolved.
 Passing the Promise every single time you call the directive can get a little much so you can wrap the directive and call the wrapper instead, like this:
 
-    import { translateWhen } from 'lit-i18n/src/lit-i18n.js';
+    import { translateWhen } from 'lit-i18n/source/lit-i18n.js';
 
     const initializePromise = i18next.use(someBackend).init(....);
     const translateDirective = (keys, options) => translateWhen(initializePromise, keys, options);
--- test/unit/lit-i18n.js
+++ test/unit/lit-i18n.js
@@ -1,117 +1,118 @@
 /* global i18next, describe, before, after, it */
 /* Jasmine will be loaded by the test framework. No need to import it. */
-import { expect } from '@esm-bundle/chai';
-import 'i18next/i18next.js';
-import 'lit-html';
-import { translate as t, html, render, registry, registryCleanup } from '../../src/lit-i18n.js';
+import { expect } from "@esm-bundle/chai";
+import "i18next/i18next.js";
+import "lit-html";
+import { html, registry, registryCleanup, render, translate as t } from "../../source/lit-i18n.js";
 
 /** i18next config */
 i18next.init({
-    lng: 'en',
-    debug: true,
-    resources: {
-        en: {
-            translation: {
-                key: 'Some translation',
-                introduceself: 'My name is {{name}}',
-                divtitle: 'Element title attribute',
-                whatishow: '{{what}} is {{how}}',
-                datamodel: '{{person.name}} is a {{person.age}} year old and is male: {{person.male}}',
-                entername: 'Enter Name',
-            },
-        },
-        fr: {
-            translation: {
-                key: 'Un peu de traduction',
-                introduceself: "Je m'appelle {{name}}",
-                divtitle: "Attribut de titre d'élément",
-                whatishow: '{{what}} est {{how}}',
-                datamodel: '{{person.name}} a {{person.age}} ans et est un homme: {{person.male}}',
-                entername: 'Entrez le nom',
-            },
-        },
+  lng: "en",
+  debug: true,
+  resources: {
+    en: {
+      translation: {
+        key: "Some translation",
+        introduceself: "My name is {{name}}",
+        divtitle: "Element title attribute",
+        whatishow: "{{what}} is {{how}}",
+        datamodel: "{{person.name}} is a {{person.age}} year old and is male: {{person.male}}",
+        entername: "Enter Name",
+      },
     },
+    fr: {
+      translation: {
+        key: "Un peu de traduction",
+        introduceself: "Je m'appelle {{name}}",
+        divtitle: "Attribut de titre d'élément",
+        whatishow: "{{what}} est {{how}}",
+        datamodel: "{{person.name}} a {{person.age}} ans et est un homme: {{person.male}}",
+        entername: "Entrez le nom",
+      },
+    },
+  },
 });
 
 /** test CustomElement */
 customElements.define(
-    'i18n-test',
-    /** Test custom element */
-    class I18nTestElement extends HTMLElement {
-        /** Constructor */
-        constructor() {
-            super();
-        }
+  "i18n-test",
+  /** Test custom element */
+  class I18nTestElement extends HTMLElement {
+    /** Constructor */
+    constructor() {
+      super();
+    }
 
-        /** Hook in to render */
-        connectedCallback() {
-            this.render();
-        }
+    /** Hook in to render */
+    connectedCallback() {
+      this.render();
+    }
 
-        /**  */
-        get renderTemplate() {
-            return html`
-                <span>${t('key')}</span>
-            `;
-        }
+    /**  */
+    get renderTemplate() {
+      return html` <span>${t("key")}</span> `;
+    }
 
-        /** Perform render */
-        render() {
-            render(this.renderTemplate, this);
-        }
-    },
+    /** Perform render */
+    render() {
+      render(this.renderTemplate, this);
+    }
+  },
 );
 
 /** @typedef {{name: string; age: number; male: boolean}} Person */
 /** More complete example */
 class I18nFull extends HTMLElement {
-    /** @returns {Person} */
-    get person() {
-        return this._person;
-    }
+  /** @returns {Person} */
+  get person() {
+    return this._person;
+  }
 
-    /** @param {Person} */
-    set person(value) {
-        this._person = value;
-        render(this.renderTemplate, this);
-    }
+  /** @param {Person} */
+  set person(value) {
+    this._person = value;
+    render(this.renderTemplate, this);
+  }
 
-    /** @inheritdoc */
-    constructor() {
-        super();
-    }
+  /** @inheritdoc */
+  constructor() {
+    super();
+  }
 
-    /** @inheritdoc */
-    connectedCallback() {
-        if (!this.person) {
-            this.person = {
-                name: 'None',
-                age: 0,
-                male: false,
-            };
-        }
+  /** @inheritdoc */
+  connectedCallback() {
+    if (!this.person) {
+      this.person = {
+        name: "None",
+        age: 0,
+        male: false,
+      };
     }
+  }
 
-    /** @returns {import('lit-html/lit-html').TemplateResult} */
-    get renderTemplate() {
-        return html`
-            <span class="basic">${t('introduceself', { name: this.person.name })}</span>
-            <div class="title" title="${t('divtitle')}">Div with translated title</div>
-            <div class="title-interpolation" title="${t('whatishow', { what: 'i18next', how: 'great' })}"></div>
-            <span class="person">${t('datamodel', { person: this.person })}</span>
-            <input class="placeholder" type="text" placeholder="${t('entername')}" />
-        `;
-    }
+  /** @returns {import('lit-html/lit-html').TemplateResult} */
+  get renderTemplate() {
+    return html`
+      <span class="basic">${t("introduceself", { name: this.person.name })}</span>
+      <div class="title" title="${t("divtitle")}">Div with translated title</div>
+      <div
+        class="title-interpolation"
+        title="${t("whatishow", { what: "i18next", how: "great" })}"
+      ></div>
+      <span class="person">${t("datamodel", { person: this.person })}</span>
+      <input class="placeholder" type="text" placeholder="${t("entername")}" />
+    `;
+  }
 }
-customElements.define('i18n-full', I18nFull);
+customElements.define("i18n-full", I18nFull);
 
 /** @type {Array<Element>} */
 let elements = [];
 
 /** Removes any added elements */
 const tidyElements = () => {
-    elements.forEach(e => e.parentElement && e.remove());
-    elements = [];
+  elements.forEach(e => e.parentElement && e.remove());
+  elements = [];
 };
 
 /**
@@ -120,90 +121,92 @@ const tidyElements = () => {
  * @returns {Element}
  */
 const addElement = tag => {
-    return elements[elements.push(document.body.appendChild(document.createElement(tag || 'i18n-test'))) - 1];
+  return elements[
+    elements.push(document.body.appendChild(document.createElement(tag || "i18n-test"))) - 1
+  ];
 };
 
 mocha.setup({
-    timeout: 15000,
+  timeout: 15000,
 });
 
 /** Tests */
-describe('Translations', () => {
-    it('Should perform translation', () => {
-        const elem = addElement('i18n-full');
-        if (elem instanceof I18nFull) {
-            const personElem = elem.querySelector('.person');
-            expect(personElem.innerText).to.equal('None is a 0 year old and is male: false');
-            elem.person = {
-                name: 'Fred',
-                age: 46,
-                male: true,
-            };
-            expect(personElem.innerText).to.equal('Fred is a 46 year old and is male: true');
-        }
-    });
+describe("Translations", () => {
+  it("Should perform translation", () => {
+    const elem = addElement("i18n-full");
+    if (elem instanceof I18nFull) {
+      const personElem = elem.querySelector(".person");
+      expect(personElem.innerText).to.equal("None is a 0 year old and is male: false");
+      elem.person = {
+        name: "Fred",
+        age: 46,
+        male: true,
+      };
+      expect(personElem.innerText).to.equal("Fred is a 46 year old and is male: true");
+    }
+  });
 
-    it('Should translate attributes', () => {
-        const titleElem = addElement('i18n-full').querySelector('.title');
-        expect(titleElem.title).to.equal('Element title attribute');
-        const intElem = addElement('i18n-full').querySelector('.title-interpolation');
-        expect(intElem.title).to.equal('i18next is great');
-        const input = addElement('i18n-full').querySelector('.placeholder');
-        expect(input.placeholder).to.equal('Enter Name');
-    });
+  it("Should translate attributes", () => {
+    const titleElem = addElement("i18n-full").querySelector(".title");
+    expect(titleElem.title).to.equal("Element title attribute");
+    const intElem = addElement("i18n-full").querySelector(".title-interpolation");
+    expect(intElem.title).to.equal("i18next is great");
+    const input = addElement("i18n-full").querySelector(".placeholder");
+    expect(input.placeholder).to.equal("Enter Name");
+  });
 });
 
-describe('Events', () => {
-    after(async () => {
-        await i18next.changeLanguage('en');
-    });
-    it('Should react to language changes', async () => {
-        const elem = addElement('i18n-full');
-        const titleElem = elem.querySelector('.title');
-        expect(titleElem.title).to.equal('Element title attribute');
-        const intElem = elem.querySelector('.title-interpolation');
-        expect(intElem.title).to.equal('i18next is great');
-        const input = elem.querySelector('.placeholder');
-        expect(input.placeholder).to.equal('Enter Name');
-        const personElem = elem.querySelector('.person');
-        elem.person = {
-            name: 'Fred',
-            age: 46,
-            male: true,
-        };
-        expect(personElem.innerText).to.equal('Fred is a 46 year old and is male: true');
+describe("Events", () => {
+  after(async () => {
+    await i18next.changeLanguage("en");
+  });
+  it("Should react to language changes", async () => {
+    const elem = addElement("i18n-full");
+    const titleElem = elem.querySelector(".title");
+    expect(titleElem.title).to.equal("Element title attribute");
+    const intElem = elem.querySelector(".title-interpolation");
+    expect(intElem.title).to.equal("i18next is great");
+    const input = elem.querySelector(".placeholder");
+    expect(input.placeholder).to.equal("Enter Name");
+    const personElem = elem.querySelector(".person");
+    elem.person = {
+      name: "Fred",
+      age: 46,
+      male: true,
+    };
+    expect(personElem.innerText).to.equal("Fred is a 46 year old and is male: true");
 
-        await i18next.changeLanguage('fr');
-        expect(titleElem.title).to.equal("Attribut de titre d'élément");
-        expect(intElem.title).to.equal('i18next est great');
-        expect(input.placeholder).to.equal('Entrez le nom');
-        expect(personElem.innerText).to.equal('Fred a 46 ans et est un homme: true');
-    });
+    await i18next.changeLanguage("fr");
+    expect(titleElem.title).to.equal("Attribut de titre d'élément");
+    expect(intElem.title).to.equal("i18next est great");
+    expect(input.placeholder).to.equal("Entrez le nom");
+    expect(personElem.innerText).to.equal("Fred a 46 ans et est un homme: true");
+  });
 });
 
-describe('Garbage collection', () => {
-    before(() => {
-        tidyElements();
-        registryCleanup();
-    });
+describe("Garbage collection", () => {
+  before(() => {
+    tidyElements();
+    registryCleanup();
+  });
 
-    after(() => {
-        tidyElements();
-    });
+  after(() => {
+    tidyElements();
+  });
 
-    it('Parts references should be released for garbage collect', done => {
-        expect(registry.size).to.equal(0);
-        for (let i = 0, iLen = 1000; i < iLen; i++) {
-            const elem = addElement();
-            if (i > 100) {
-                elem.remove();
-            }
-        }
+  it("Parts references should be released for garbage collect", done => {
+    expect(registry.size).to.equal(0);
+    for (let i = 0, iLen = 1000; i < iLen; i++) {
+      const elem = addElement();
+      if (i > 100) {
+        elem.remove();
+      }
+    }
 
-        expect(registry.size).to.equal(1000);
-        setTimeout(() => {
-            expect(registry.size).to.equal(101);
-            done();
-        }, 11000);
-    });
+    expect(registry.size).to.equal(1000);
+    setTimeout(() => {
+      expect(registry.size).to.equal(101);
+      done();
+    }, 11000);
+  });
 });
